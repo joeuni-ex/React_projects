b7c65bbf5772afdd86b82f2356e673d64f490941
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-29 14:04:52 +0900

    data.js 데이터 추가

--- src/data.js
+++ src/data.js
@@ -2,6 +2,10 @@ import p1 from "./assets/movieApp.png";
 import p2 from "./assets/imageGallery.png";
 import p3 from "./assets/cookingRecipe.png";
 import p4 from "./assets/taskApp.png";
+import p5 from "./assets/magicCard.png";
+import p6 from "./assets/imageGallery2.png";
+import p7 from "./assets/taskApp.png";
+import p8 from "./assets/taskApp.png";
 
 export const projects = [
   {
@@ -27,5 +31,29 @@ export const projects = [
     title: "TaskApp",
     url: "https://react-vite-projects-4-accordion.netlify.app/",
     image: p4,
+  },
+  {
+    id: 5,
+    title: "MagicCardGame",
+    url: "https://react-magicgame-joeuni.netlify.app/",
+    image: p5,
+  },
+  {
+    id: 6,
+    title: "ImageGallery2",
+    url: "https://react-imagegallery-joeuni.netlify.app/",
+    image: p6,
+  },
+  {
+    id: 7,
+    title: "TaskApp",
+    url: "https://react-vite-projects-4-accordion.netlify.app/",
+    image: p7,
+  },
+  {
+    id: 8,
+    title: "TaskApp",
+    url: "https://react-vite-projects-4-accordion.netlify.app/",
+    image: p8,
   },
 ];
