accb0832fe494fa3c2f67101519c8cc81f06acc7
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-29 11:31:12 +0900

    컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Hero from "./Hero";
+import Projects from "./Projects";
+
 function App() {
-  return <>앱</>;
+  return (
+    <main>
+      <Hero />
+      <Projects />
+    </main>
+  );
 }
 
 export default App;
