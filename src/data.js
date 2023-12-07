410443a5ad4e8028903018195e56ba41e633049f
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-07 15:16:00 +0900

    리액트 프로젝트 추가

--- src/data.js
+++ src/data.js
@@ -6,54 +6,88 @@ import p5 from "./assets/magicCard.png";
 import p6 from "./assets/imageGallery2.png";
 import p7 from "./assets/backroads.png";
 import p8 from "./assets/movieApp2.png";
+import p9 from "./assets/firebaseTodo.png";
+import p10 from "./assets/superChat.png";
+import p11 from "./assets/Mymoney.png";
+import p12 from "./assets/githubFinder.png";
 
 export const projects = [
   {
     id: 1,
-    title: "MovieApp",
+    title: "Movie App",
     url: "https://react-newmovieapp-joeuni.netlify.app/",
     image: p1,
   },
   {
     id: 2,
-    title: "ImageGalleryApp",
+    title: "ImageGallery App",
     url: "https://react-imagegallery-joeuni89.netlify.app/",
     image: p2,
   },
   {
     id: 3,
-    title: "CookingRecipeApp",
-    url: "https://react-cooking-recipe-app-joeuni.netlify.app/",
-    image: p3,
+    title: "MytodoList App",
+    url: "https://react-firebase9-todolist-joeuni.netlify.app/",
+    image: p9,
   },
   {
     id: 4,
-    title: "TaskApp",
-    url: "https://taskapp-joeuni.netlify.app/",
-    image: p4,
+    title: "SuperChat App",
+    url: "https://react-superchat-joeuni.netlify.app/",
+    image: p10,
   },
+
   {
     id: 5,
-    title: "MagicCardGameApp",
-    url: "https://react-magicgame-joeuni.netlify.app/",
-    image: p5,
+    title: "MyMoney App",
+    url: "https://react-my-money-app-joeuni.netlify.app/",
+    image: p11,
   },
+
   {
     id: 6,
-    title: "ImageSearchApp",
-    url: "https://react-imagegallery-joeuni.netlify.app/",
-    image: p6,
+    title: "GithubFinder App",
+    url: "https://react-github-app-joeuni.netlify.app/",
+    image: p12,
   },
+
   {
     id: 7,
-    title: "BackRoadsApp",
+    title: "BackRoads App",
     url: "https://react-backroads-joeuni.netlify.app/",
     image: p7,
   },
   {
     id: 8,
-    title: "MovieSearchApp",
+    title: "CookingRecipe App",
+    url: "https://react-cooking-recipe-app-joeuni.netlify.app/",
+    image: p3,
+  },
+  {
+    id: 9,
+    title: "MovieSearch App",
     url: "https://react-movieapp-joeuni.netlify.app/",
     image: p8,
+  },
+
+  {
+    id: 10,
+    title: "Task App",
+    url: "https://taskapp-joeuni.netlify.app/",
+    image: p4,
+  },
+
+  {
+    id: 11,
+    title: "MagicCardGame App",
+    url: "https://react-magicgame-joeuni.netlify.app/",
+    image: p5,
+  },
+
+  {
+    id: 12,
+    title: "ImageSearch App",
+    url: "https://react-imagegallery-joeuni.netlify.app/",
+    image: p6,
   },
 ];
